6e4adbe1924ec0d0e2407309c8d51e838d0b494c
hjy <heojungyeon.97@gmail.com>
2022-01-13 18:26:00 +0900

    + 프로그래머스 위장 java

---
+++ hash/위장/Solution.java
@@ -0,0 +1,13 @@
+// 프로그래머스 위장 java
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream()
+                .collect(reducing(1L, (x, y) -> x * (y + 1))).intValue() - 1;
+    }
+}
